d667c1d0b5267d61c53fe07e97ba06499f1a206c
Firzus <lprieu@gmail.com>
2022-07-18 16:14:04 +0200

    end contact

--- website/src/pages/Contact.js
+++ website/src/pages/Contact.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import Header from '../components/Header';
-import { AiFillLinkedin, AiTwotoneMail, AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin, AiFillMail, AiFillGithub } from "react-icons/ai";
+import { Link } from 'react-router-dom';
+
+const ColoredLine = ({ color }) => (
+    <hr
+        style={{
+            color: color,
+            backgroundColor: color,
+            height: 5
+        }}
+    />
+);
 
 class Contact extends Component {
+
     render() {
         return (
             <>
@@ -12,85 +23,109 @@ class Contact extends Component {
 
                     <Header as={Row} />
 
-                    <Container className='flex-grow-1 d-flex align-items-center'>
+                    <Container className='flex-grow-1 d-block d-sm-flex align-items-center'>
 
-                        <Col>
+                        <Col className=''>
 
-                            <Row className='p-5 text-center text-white font-link fs-1'>
+                            <Row className='p-5 d-flex justify-content-center text-white font-link fs-1'>
                                 Contact
                             </Row>
 
                             <Row>
 
-                                <Col className='flex-column'>
+                                <Row className='d-flex align-items-center'>
+                                    <Col className='p-3 d-flex justify-content-end'>
 
-                                    <Row>
-                                        <a target="_blank" href='https://www.google.com/'>
+                                        <a href='https://www.google.com/'>
 
-                                            <AiTwotoneMail
-                                                className='p-2'
-                                                size='5em'
+                                            <AiFillMail
+                                                size='4em'
                                                 color='white'
                                             />
 
                                         </a>
-                                    </Row>
 
-                                    <Row>
-                                        <a target="_blank" href='https://www.google.com/'>
+                                    </Col>
 
-                                            <AiTwotoneMail
-                                                className='p-2'
-                                                size='5em'
-                                                color='white'
-                                            />
-
-                                        </a>
-                                    </Row>
-
-                                    <Row>
-                                        <a target="_blank" href='https://www.google.com/'>
-
-                                            <AiTwotoneMail
-                                                className='p-2'
-                                                size='5em'
-                                                color='white'
-                                            />
-
-                                        </a>
-                                    </Row>
-
-                                </Col>
-
-                                <Col className='flex-column text-white'>
-
-                                    <div
-                                        className='text-white fs-3 p-2 font-link'
+                                    <Col
+                                        className='text-white fs-3 p-3 font-link'
                                     >
                                         Email
-                                    </div>
+                                    </Col>
+                                </Row>
 
-                                    <div
-                                        className='text-white fs-3 p-2 font-link'
+                                <Row className='d-flex align-items-center'>
+                                    <Col className='p-3 d-flex justify-content-end'>
+
+                                        <a href='https://www.linkedin.com/in/lilian-prieu-554058221/'>
+
+                                            <AiFillLinkedin
+                                                size='4em'
+                                                color='white'
+                                            />
+
+                                        </a>
+
+                                    </Col>
+
+                                    <Col
+                                        className='text-white fs-3 p-3 font-link'
                                     >
                                         Linkedin
-                                    </div>
+                                    </Col>
+                                </Row>
 
-                                    <div
-                                        className='text-white fs-3 p-2 font-link'
+                                <Row className='d-flex align-items-center'>
+                                    <Col className='p-3 d-flex justify-content-end'>
+
+                                        <a href='https://github.com/Firzus'>
+
+                                            <AiFillGithub
+                                                size='4em'
+                                                color='white'
+                                            />
+
+                                        </a>
+
+                                    </Col>
+
+                                    <Col
+                                        className='text-white fs-3 p-3 font-link'
                                     >
                                         GitHub
-                                    </div>
-
-                                </Col>
+                                    </Col>
+                                </Row>
 
                             </Row>
 
                         </Col>
 
-                        <Col className='flex-column'>
+                        <Col className='d-flex align-items-center justify-content-center'>
 
-                            part 2
+                            <Row className='d-flex align-items-center justify-content-center'>
+
+                                <Row className='d-flex justify-content-center fs-1 text-white text-center font-link p-5'>
+                                    Curriculum Vitae
+                                </Row>
+
+                                <Row className='d-flex justify-content-center align-items-center w-50 p-2'>
+
+                                    <Button
+                                        variant="outline-light"
+                                        size="lg"
+                                        to="/files/cv_prieu_lilian.pdf"
+                                        target="_blank"
+                                        download
+                                        as={Link}
+                                    >
+
+                                        Télécharger
+
+                                    </Button>
+
+                                </Row>
+
+                            </Row>
 
                         </Col>
 
